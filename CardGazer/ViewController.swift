a05cc9c11c890c62feb3e2e5a1f1333071ddbbec
ClockworkPirate <mguerrieri@outlook.com>
2014-10-04 01:25:28 -0700

    First commit

---
+++ CardGazer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CardGazer
+//
+//  Created by Star-Lord on 10/4/14.
+//  Copyright (c) 2014 Igneus. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
